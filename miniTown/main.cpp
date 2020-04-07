36fa38db5e66df6eecf7f7ff44b3fcfa678babd1
YuFeiran <yufeiran1999@gmail.com>
2020-04-07 11:49:28 +0800

    重建

---
+++ miniTown/main.cpp
@@ -0,0 +1,353 @@
+#include"miniTown.h"
+using namespace std;
+
+bool clkClick = false; //每过一整秒就设一下为true
+
+Picture picLand;
+Picture picHouse;
+Picture picHouse1;
+Picture picFarmer;
+Picture picBuilder;
+Picture picField;
+Picture picField1;
+Picture picField2;
+Picture picTree;
+Picture picKing;
+Picture picRice;
+Picture picWood;
+Picture picSun;
+Picture picBackground;
+Picture picBackground1;
+Picture picMoon;
+Picture picKingHouse;
+Picture picLove;
+Picture picChild;
+
+
+int NowHouseSum = 0;
+int NowFieldSum = 0;
+int NowTreeSum = 0;
+int NowRiceSum = 0;
+int NowWoodSum = 0;
+int NowFarmerSum = 0;
+int NowBuilderSum = 0;
+int NowChildSum = 0;
+Object objHouse[MaxHouseSum];
+Object objFarmer[MaxFarmerSum];
+Object objBuilder[MaxBuilderSum];
+Object objChild[MaxChildSum];
+Object objField[MaxFieldSum];
+Object objRice[MaxRiceSum];
+Object objTree[MaxTreeSum];
+Object objWood[MaxWoodSum];
+Object objBackground;
+
+Object objKing;
+
+Object objSun;
+Object objMoon;
+Object objLove;
+
+Farmer farmer[MaxFarmerSum];
+Builder builder[MaxBuilderSum];
+Child child[MaxChildSum];
+King king;
+Field field[MaxFieldSum];
+Tree tree[MaxTreeSum];
+House house[MaxHouseSum];
+
+
+
+float runtime = 0;
+float DayTimeNow = 0; //今天的进度(0~DayTime)
+float DayTimeNowRate = 0; //今天的进度(0~1)
+int DaySum = 0; //经过的日子数量
+float timeScale = 1;
+int LastFPS = 0;
+float FrameTime = 0; //当前帧间隔时间
+int frame = 0; //帧率
+
+void initLoadPic()
+{
+	picLand.loadImage("pic/land.bmp");
+	picHouse.loadImage("pic/house.bmp");
+	picHouse1.loadImage("pic/house1.bmp");
+	picFarmer.loadImage("pic/farmer.bmp");
+	picBuilder.loadImage("pic/builder.bmp");
+	picField.loadImage("pic/field.bmp");
+	picTree.loadImage("pic/tree.bmp");
+	picKing.loadImage("pic/king.bmp");
+	picField1.loadImage("pic/field1.bmp");
+	picField2.loadImage("pic/field2.bmp");
+	picRice.loadImage("pic/rice.bmp");
+	picWood.loadImage("pic/wood.bmp");
+	picSun.loadImage("pic/sun.bmp");
+	picBackground.loadImage("pic/background.bmp");
+	picBackground1.loadImage("pic/background1.bmp");
+	picMoon.loadImage("pic/moon.bmp");
+	picKingHouse.loadImage("pic/kinghouse.bmp");
+	picLove.loadImage("pic/love.bmp");
+	picChild.loadImage("pic/child.bmp");
+
+	cout << "Load Pic OK!" << endl;
+}
+
+void initObject()
+{
+	srand(time(NULL));
+	for (int i = 1; i < 4; i++)
+	{
+		AddField(1, i);
+		AddFinishHouse(2, i);
+		AddFarmer(2, i,1);
+		
+	}
+
+	for (int i = 1; i < 3; i++)
+	{
+		AddTree(3, i);
+		AddFinishHouse(4, i);
+		AddBuilder(4, i,0);
+		AddUnFinishHouse(5, i);
+	}
+
+	AddTree(-1, 5);
+
+	AddFinishHouse(8, 2,1);
+	AddKing(8, 2);
+	king.money = 100;
+
+
+	objBackground.pic = &picBackground;
+	objBackground.point.x = 0;
+	objBackground.point.y = 0;
+	AddDrawObject(&objBackground);
+
+	objSun.pic = &picSun;
+	objSun.x = 0;
+	objSun.y = 0;
+	//AddDrawObject(&objSun);
+
+	objMoon.pic = &picMoon;
+	objMoon.x = -objMoon.pic->getWidth();
+	objMoon.y = 0;
+	AddDrawObject(&objMoon);
+
+
+	objLove.pic = &picLove;
+	objLove.x = 0;
+	objLove.y = 0;
+
+}
+
+int main()
+{
+	Player player;
+	player.playSoundLoop("sound/01 Bloom.wav");
+
+
+
+	//playMusic("sound/01 Bloom.wav");
+	//const char* title = _T("miniTown");
+	if (_CreateWindow(title, nScreenWidth, nScreenHeight))
+	{
+		return -1;
+	}
+	
+	cout << "miniTown" << endl;
+
+	//initLoadPic();//_CreateWindow执行完之后会加载图片
+	initObject();
+
+	clock_t start, stop;
+	clock_t OneSecondTime = clock();
+
+	start = clock();
+	
+	//cout << "Time scale?";
+
+	//cin >> timeScale;
+	timeScale = 2;
+	
+	while (1)
+	{
+
+		KeyControl();
+		//MsgGet();加入到了Draw()
+		Draw();
+		
+
+		AILoop();
+		ShowSky();
+		
+		frame++;
+		stop = clock();
+		FrameTime = (stop - start) * 1.0 / CLOCKS_PER_SEC * timeScale;
+		if (FrameTime > 0.1) //防止暂停
+		{
+			FrameTime = 0;
+		}
+		runtime += FrameTime;
+		DayTimeNow+= FrameTime;
+
+		if (DayTimeNow > DayTime)
+		{
+			DayTimeNow = 0;
+			cout << "Day Out!" << endl;
+		}
+		DayTimeNowRate = DayTimeNow / (float)DayTime;
+		start = stop;
+		if (stop - OneSecondTime >= CLOCKS_PER_SEC)
+		{
+			OneSecondTime = stop;
+			clkClick = true;
+
+			DaySum = (int)runtime / DayTime;
+			//cout << DaySum << endl;
+			char title[200];
+			sprintf_s(title, "FPS %d ", frame);
+			SetWindowText(hWnd, title);
+			LastFPS = frame;
+			frame = 0;
+			//cout << "run time:" << runtime << endl;
+		}
+		else
+		{
+			clkClick = false;
+		}
+
+	}
+
+	return 0;
+}
+
+
+
+
+void KeyControl()
+{
+	static int pressL = false;
+	static int pressR = false;
+	static int pressH = false;
+	static int pressM = false;
+	static int pressN = false;
+	static int pressB = false;
+	int speed = 40; //村长的移动速度
+	if (screen_keys[VK_ESCAPE])
+	{
+		//在这里调用其他模块的释放函数
+		freeSomethingForEngine();
+		exit(1);		//正常结束
+	}
+	 if (screen_keys['W'])
+	{
+		king.DrawObject->y-= (float)timeScale * speed * FrameTime;
+	}
+	if (screen_keys['S'])
+	{
+		king.DrawObject->y+= (float)timeScale * speed * FrameTime;
+	}
+	if (screen_keys['A'])
+	{
+		king.DrawObject->x-= (float)timeScale * speed * FrameTime;
+	}
+	if (screen_keys['D'])
+	{
+		king.DrawObject->x+= (float)timeScale * speed * FrameTime;
+	}
+	if (screen_keys['Q']) {
+		int angle = king.DrawObject->getAngle();
+		king.DrawObject->setAngle(angle + 5);
+
+	}
+	if (screen_keys['E']) {
+		int angle = king.DrawObject->getAngle();
+		king.DrawObject->setAngle(angle - 5);
+
+	}
+	if (screen_keys['L'])  //list 显示当前村民资源
+	{
+		if (pressL == false)
+		{
+			ResourceCount();
+		}
+		pressL = true;
+	}
+	else
+	{
+		pressL = false;
+	}
+	if (screen_keys['R'])
+	{
+		if (pressR == false)
+		{
+			cout << "Enter Rice Price:";
+			int price;
+			cin >> price;
+			king.SetRicePrice(price);
+		}
+		pressR = true;
+	}
+	else
+	{
+		pressR = false;
+	}
+	if (screen_keys['H'])
+	{
+		if (pressH == false)
+		{
+			cout << "Enter House Price:";
+			int price;
+			cin >> price;
+			king.SetHousePrice(price);
+		}
+		pressH = true;
+	}
+	else
+	{
+		pressH = false;
+	}
+	if (screen_keys['M'])
+	{
+		if (pressM == false)
+		{
+			cout << "Enter Sum of the Money Add:";
+			int sum;
+			cin >> sum;
+			king.MakeMoney(sum);
+		}
+		pressM = true;
+	}
+	else
+	{
+		pressM = false;
+	}
+	if (screen_keys['N'])
+	{
+		if (pressN == false)
+		{
+			cout << "Enter Sum of the Money Destory:";
+			int sum;
+			cin >> sum;
+			king.DestoryMoney(sum);
+		}
+		pressN = true;
+	}
+	else
+	{
+		pressN = false;
+	}
+	if (screen_keys['B'])
+	{
+		if (pressB == false)
+		{
+			king.SetUnFinishHouseMark();
+		}
+		pressB = true;
+	}
+	else
+	{
+		pressB = false;
+	}
+
+}
---
+++ miniTown/miniTown.h
@@ -0,0 +1,387 @@
+//
+//
+//  miniTown
+//
+//
+//=============================================================================
+
+
+#pragma once
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
+#include<iostream>
+#include<time.h>
+#include<tchar.h>
+#include"Debug.h"
+#include"Engine.h"
+
+
+
+//----------------------------------
+//调试使用的全局变量
+const bool DebugShowGrowTimeFlag = false;  //输出田和树木的生长、砍伐时间
+
+//-----------------------------------
+
+
+//======================================================
+// 游戏引擎部分
+
+//用于显示窗体的全局变量
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 600;
+const int bits = 24;
+//extern BYTE buffer[SCREEN_WIDTH * SCREEN_HEIGHT * bits / 8];
+
+class coord //以60*60为单位标记坐标
+{
+public:
+	coord(int X, int Y) :x(X), y(Y) {};
+	coord() :x(0), y(0) {};
+	int x, y;
+};
+
+
+
+void CleanScreen();
+void Draw();
+const int MaxDrawObjectSum = 100000;
+extern Object* drawList[MaxDrawObjectSum];
+extern int drawSum;
+
+void AddDrawObject(Object* object);
+void RemoveDrawObecjt(Object* object);
+
+
+void KeyControl();
+
+
+
+extern float runtime;
+extern int LastFPS;
+extern int frame; //当前的帧率
+extern float timeScale;
+
+
+
+const int MaxHouseSum = 1000;
+const int MaxFieldSum = 1000;
+const int MaxFarmerSum = 1000;
+const int MaxBuilderSum = 1000;
+const int MaxChildSum = 1000;
+const int MaxTreeSum = 1000;
+const int MaxRiceSum = 1000;
+const int MaxWoodSum = 1000;
+const int MaxObjectSum = 1000;
+
+
+extern int FieldProduceRiceSum;
+extern int RicePrice;
+extern int HousePrice;
+extern int FirstPayHousePrice; //盖房子预先给木匠的定金
+extern int ChildGrowDayTime;
+
+extern float DayTimeNowRate; //今天的进度(0~1)
+extern float DayTimeNow;
+extern float FrameTime; //当前帧间隔时间
+extern int DaySum; 
+
+class Field
+{
+public:
+	int id;
+	bool isUsed = false;
+	float growingTime;
+	Object *DrawObject;
+	void AddRice();
+	void AI();
+};
+
+class Rice
+{
+public:
+	int id;
+	Object* DrawObject;
+};
+
+class House
+{
+public:
+	int id;
+	float buildTime=0;
+	const int RequireBuildTime = 30;
+	int FirstBuildMoney = 0; //盖房子预付给木匠的钱
+	bool isKingHouse = false;
+	bool isUsed = false;
+	bool isOnBuild = false;
+	Object *DrawObject;
+	Object* StoneRice[MaxRiceSum];
+	Object* StoneWood[MaxWoodSum];
+	int StoneRiceSum;
+	int StoneWoodSum;
+
+};
+
+class Tree
+{
+public:
+	int id;
+	Object* DrawObject;
+	float cutTime = 0;
+	void AddWood();
+};
+
+class FamilyTree;
+
+class Farmer
+{
+public:
+	int id;
+	int Sex; //男的为1 女的为0
+	float age;
+	int money;
+	Field* belongField;
+	House* belongHouse;
+	House* ownHouse = NULL; //给后代的房子
+	FamilyTree* familyTree = NULL;
+	bool isCanMarriage = false;
+	bool isMarriage = false;
+	Object* DrawObject;
+	int wantFoodLevel;
+	float wantSexLevel;
+	bool isDead = false;
+	bool isTryBuyHouse = false;
+	Object* TakeOnThing[MaxObjectSum];
+	int TakeOnThingSum = 0;
+	void WalkTo(Object* object);
+	void Eat();
+	void Sleep();
+	void GrowRice();
+	void AI();
+	void PutRice();
+	void GetARiceToHand();  //从房子里拿出一个水稻到手上
+	void GetAllRiceToHand(); //从房子里拿出所有水稻到手上
+
+	bool BuyHouse();
+	void judgeDead();
+
+	bool SellRiceForMoney();
+
+	int LastDaySum=0; //用来计算食欲的临时变量
+	
+};
+
+class Builder
+{
+public:
+	int id;
+	float age;
+	int money;
+	int Sex; //男的为1 女的为0
+	House* belongHouse;
+	Tree* AimTree=NULL;
+	House* AimUnFinishHouse = NULL;
+	House* ownHouse=NULL; //给后代的房子
+	FamilyTree* familyTree = NULL;
+	bool isCanMarriage = false;
+	bool isMarriage = false;
+
+	
+	int OwnHouseCount = 0;   //拥有的还没卖出去的房子数量
+	int wantFoodLevel;
+	float wantSexLevel;
+	int buildTime = 0;
+	bool isDead = false;
+	Object* DrawObject;
+	Object* TakeOnThing[MaxObjectSum];
+	int TakeOnThingSum = 0;
+	void WalkTo(Object* object);
+	void BuildHouse();
+	void Eat();
+	void Sleep();
+	void CutTree();
+	Tree* FindATree();
+	House* FindAUnFinishHouse();
+	void AI();
+	void PutWood();
+	bool BuyRice();
+	bool BuyHouse();
+	bool PutRice();
+	void judgeDead();
+	
+
+	bool HouseForMoney();
+	int LastDaySum = 0; //用来计算食欲的临时变量
+	//买水稻用的临时变量
+	bool isTryBuyRice = false;
+	bool isBuyRiceFinish = false;
+	//买水稻用的临时变量
+	bool isTryBuyHouse = false;
+};
+
+class King
+{
+public:
+	int id;
+	float age;
+	int Sex; //男的为1 女的为-1
+	int money;
+	House* ownHouse = NULL; //给后代的房子
+	House* belongHouse;
+	Object* DrawObject;
+	int wantFoodLevel;
+	float wantSexLevel;
+	Object* TakeOnThing;
+	int HaveEmptyHouseSum=0;
+	void WalkTo(Object* object);
+	
+	void AI();
+	void MakeMoney(int Sum);
+	void DestoryMoney(int Sum);
+	void SetRicePrice(int Price);
+	void SetHousePrice(int Price);
+	bool SetUnFinishHouseMark();  //在村长当前位置设置一个盖房子的记号（木匠随后会取盖房子）需要村长花一部分钱
+
+};
+
+class Child
+{
+public:
+	int id;
+	int age;
+	int sex;
+	bool isDead = false;
+	House* belongHouse = NULL;
+	Object* DrawObject;
+	int wantFoodLevel;
+	void GrowUp();
+	void Walk(); //随机在地图里散步
+	void Sleep();
+	void Eat();
+	void AI();
+};
+
+
+class FamilyTree
+{
+public:
+	int FatherType;  //父亲的职业类型
+	int MotherType;  //母亲的职业类型
+	Builder *Father0;
+	Farmer *Father1;
+	King *Father2;
+	Builder *Mother0;
+	Farmer *Mother1;
+	King *Mother2;
+	int ChildType;
+};
+
+const int DayTime = 30;
+
+//extern Picture picLand;
+//extern Picture picHouse;
+//extern Picture picHouse1;
+//extern Picture picFarmer;
+//extern Picture picBuilder;
+//extern Picture picField;
+//extern Picture picField1;
+//extern Picture picField2;
+//extern Picture picTree;
+//extern Picture picKing;
+//extern Picture picRice;
+//extern Picture picWood;
+//extern Picture picSun;
+//extern Picture picBackground;
+//extern Picture picBackground1;
+//extern Picture picMoon;
+//extern Picture picKingHouse;
+
+extern Picture picLand;
+extern Picture picHouse;
+extern Picture picHouse1;
+extern Picture picFarmer;
+extern Picture picBuilder;
+extern Picture picField;
+extern Picture picField1;
+extern Picture picField2;
+extern Picture picTree;
+extern Picture picKing;
+extern Picture picRice;
+extern Picture picWood;
+extern Picture picSun;
+extern Picture picBackground;
+extern Picture picBackground1;
+extern Picture picMoon;
+extern Picture picKingHouse;
+extern Picture picChild;
+
+
+extern int NowHouseSum;
+extern int NowFieldSum;
+extern int NowTreeSum;
+extern int NowRiceSum;
+extern int NowWoodSum;
+extern int NowFarmerSum;
+extern int NowBuilderSum;
+extern int NowChildSum;
+
+extern Object objHouse[MaxHouseSum];
+extern Object objFarmer[MaxFarmerSum];
+extern Object objBuilder[MaxBuilderSum];
+extern Object objChild[MaxChildSum]; 
+extern Object objField[MaxFieldSum];
+extern Object objRice[MaxRiceSum];
+extern Object objTree[MaxTreeSum];
+extern Object objWood[MaxWoodSum];
+extern Object objKing;
+extern Object objSun;
+extern Object objMoon;
+extern Object objBackground;
+extern Object objLove;
+
+extern Field field[MaxFieldSum];
+extern Tree tree[MaxTreeSum];
+extern House house[MaxHouseSum];
+extern Farmer farmer[MaxFarmerSum];
+extern Builder builder[MaxBuilderSum];
+extern Child child[MaxChildSum];
+extern King king;
+
+
+//--------------------------------
+// AI
+void AILoop();
+
+
+
+//--------------------------------
+//游戏机制
+
+extern bool clkClick;
+void AddUnFinishHouse(int x, int y);
+void AddFinishHouse(int x, int y,int type=0);
+void AddFarmer(int x,int y,int sex);
+void AddBuilder(int x,int y,int sex);
+void AddChild(int x, int y, int sex,FamilyTree *familyTree);
+void AddTree(int x,int y);
+void AddField(int x, int y);
+void AddKing(int x, int y,int sex=0);
+bool ObjectIsRice(Object* object);
+bool ObjectIsWood(Object* object);
+void MakeBaby(FamilyTree* familyTree);
+
+
+void ShowSky();
+
+
+void ResourceCount();//对村民拥有的资源进行统计
+
+bool IsCloseTo(Object* A, Object* B);  //判断两个Object是否靠近
+bool IsMoreCloseTo(Object* A, Object* B); //判断两个Object是否更靠近
+float DistanceAToB(Object* A, Object* B);
+House* GetANearEmptyHouse(Object* man,int type=0); 
+Field* GetANearUnUsedField(Object* man);
+House* FindKingHouse();
+Point GetPoint(Object* object); //获取最接近的坐标
+bool IsPointUsed(Point point);
